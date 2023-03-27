ec7be4e1034b8b1dc96a8f2085abe57328c0c931
Lendri Duarte <lduarte@dmtradingve.com>
2023-03-27 15:24:22 -0300

    Agregado .env al ignore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const app = require('./server')
+require('dotenv').config();
 
-app.listen(3000, () =>{
-    console.log('Server listen on port 3000')
+const app = require('./server')
+require('./database')
+
+app.listen(app.get('port'), () =>{
+    console.log('Server listen on port', app.get('port'))
 })
--- src/server.js
+++ src/server.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const path = require('path');
+
+//Initializations
 
 const app = express();
+
+// settings
+
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares
+
+app.use(express.urlencoded({extended: false}));
+
+
+// Global Variables
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+// Static Files 
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app; 
