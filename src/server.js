b08ce254f70cef203930697524bbd0743f010a8d
Lendri Duarte <lduarte@dmtradingve.com>
2023-03-28 10:01:51 -0300

    Agregadas plantillas de hbs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
+
 
 //Initializations
 
@@ -9,6 +11,13 @@ const app = express();
 
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 
@@ -19,7 +28,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 })
 
 // Static Files 
